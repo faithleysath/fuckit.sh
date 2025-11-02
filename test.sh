0e30d065ed947ce06742da1e83741d4cee12c568
faithleysath <faithleysath@gmail.com>
2025-11-02 19:16:22 +0800

    wip

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Written in [Amber](https://amber-lang.com/)
+# version: 0.4.0-alpha
+# date: 2025-11-02 19:09:08
+__0_a="
+echo 123
+\"
+echo 456
+"
+echo "${__0_a}";
+__AS=$?
